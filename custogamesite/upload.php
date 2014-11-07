4a0a710cf526ca489a99fb8fca8db90376b9d2d1
NathanQj <nathan.qjm@gmail.com>
2014-11-07 12:03:02 +0100

    Upload des vidéos fonctionnel
    Upload de vidéos dans une limite de 200Mo pour l'instant (modifié depuis
    php.ini -> upload_max_filesize et post_max_size
    Redirection de la vidéo postée dans le dossier : upload/

--- custogamesite/upload.php
+++ custogamesite/upload.php
@@ -10,21 +10,45 @@ require('config/session.php');
 ********************************/
 if (utilisateur_est_connecte()) 
 {
-	if ($_FILES['icone']['error'] > 0) $erreur = "Erreur lors du transfert";
-	
-	if ($_FILES['icone']['size'] > $maxsize) $erreur = "Le fichier est trop gros";
-	
-	$extensions_valides = array( 'mp4' , 'avi' , 'mkv' , 'mpg' );
-	//1. strrchr renvoie l'extension avec le point (« . »).
-	//2. substr(chaine,1) ignore le premier caractère de chaine.
-	//3. strtolower met l'extension en minuscules.
-	$extension_upload = strtolower(  substr(  strrchr($_FILES['icone']['name'], '.')  ,1)  );
-	
-	if ( in_array($extension_upload,$extensions_valides) ) echo "Extension correcte";
-	$image_sizes = getimagesize($_FILES['icone']['tmp_name']);
-	
-	if ($image_sizes[0] > $maxwidth OR $image_sizes[1] > $maxheight) $erreur = "Image trop grande";
+	//si l'utilisateur valide le formulaire
+	if(isset($_POST) && !empty($_POST)){
 
+		$dossier = 'upload/';
+		$fichier = basename($_FILES['custo']['name']);
+		$taille_maxi = 25000000000;
+		$taille = filesize($_FILES['custo']['tmp_name']);
+		$extensions = array('.png', '.gif', '.jpg', '.jpeg', '.avi', '.wmv', '.mov', '.h264', '.flv', '.rmvb',);
+		$extension = strrchr($_FILES['custo']['name'], '.'); 
+		//Début des vérifications de sécurité...
+		if(!in_array($extension, $extensions)) //Si l'extension n'est pas dans le tableau
+		{
+			$erreur = 'Vous devez uploader un fichier de type png, gif, jpg, jpeg, txt ou doc...';
+		}
+		if($taille>$taille_maxi)
+		{
+			$erreur = 'Le fichier est trop gros...';
+		}
+		if(!isset($erreur)) //S'il n'y a pas d'erreur, on upload
+		{
+			//On formate le nom du fichier ici...
+			$fichier = strtr($fichier, 
+			'ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ', 
+			'AAAAAACEEEEIIIIOOOOOUUUUYaaaaaaceeeeiiiioooooouuuuyy');
+			$fichier = preg_replace('/([^.a-z0-9]+)/i', '-', $fichier);
+			if(move_uploaded_file($_FILES['custo']['tmp_name'], $dossier . $fichier)) //Si la fonction renvoie TRUE, c'est que ça a fonctionné...
+			{
+			echo 'Upload effectué avec succès !';
+			}
+			else //Sinon (la fonction renvoie FALSE).
+			{
+			echo 'Echec de l\'upload !';
+			}
+		}
+		else
+		{
+			 echo $erreur;
+		}
+	}
 }
 else
 {
