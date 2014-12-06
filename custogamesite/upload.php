3a7bc456ddd9d81ac7723f06cc632cc732ef1446
NathanQj <nathan.qjm@gmail.com>
2014-12-06 18:15:30 +0100

    Avancement de l'intégration : Upload2
    Création des matières et classes pour l'organisation des vidéos.
    Essais de lecteurs vidéos -> Problèmes par rapport au peu de formats
    lisibles.

--- custogamesite/upload.php
+++ custogamesite/upload.php
@@ -34,12 +34,12 @@ if (utilisateur_est_connecte())
 		$fichier = basename($_FILES['custo']['name']);
 		$taille_maxi = 25000000000;
 		$taille = filesize($_FILES['custo']['tmp_name']);
-		$extensions = array('.png', '.gif', '.jpg', '.jpeg', '.avi', '.wmv', '.mov', '.h264', '.flv', '.rmvb',);
+		$extensions = array('.avi', '.wmv', '.mov', '.h264', '.flv', '.rmvb', '.mp4');
 		$extension = strrchr($_FILES['custo']['name'], '.'); 
 		//Début des vérifications de sécurité...
 		if(!in_array($extension, $extensions)) //Si l'extension n'est pas dans le tableau
 		{
-			$erreur = 'Vous devez uploader un fichier de type png, gif, jpg, jpeg, txt ou doc...';
+			$erreur = 'Vous devez uploader un fichier de type avi, wmv, mov, h264, flv ou mp4...';
 		}
 		if($taille>$taille_maxi)
 		{
@@ -52,20 +52,23 @@ if (utilisateur_est_connecte())
 			'ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ', 
 			'AAAAAACEEEEIIIIOOOOOUUUUYaaaaaaceeeeiiiioooooouuuuyy');
 			$fichier = preg_replace('/([^.a-z0-9]+)/i', '-', $fichier);
-			if(move_uploaded_file($_FILES['custo']['tmp_name'], $dossier . $fichier)) 
-			//Si la fonction renvoie TRUE, c'est que le fichier a bien été envoyé dans le dossier.
+			if(!move_uploaded_file($_FILES['custo']['tmp_name'], $dossier . $fichier)) 
+			//Si la fonction renvoie FALSE, c'est que le fichier a bien été envoyé dans le dossier.
+				$erreur = 'Le fichier n\'a pas été bien enregistré';
 			{
 				if (isset($_POST['title']) && !empty($_POST['title'])
 					&& isset($_POST['description']) && !empty($_POST['description']))
-				{				
-							$request = $db->prepare('INSERT INTO videos (filename, title, description, date, auteur, auteur_id, validation) 
-														VALUES (:filename, :title, :description, now(), :auteur, :auteur_id, :validation)');
+				{
+							$request = $db->prepare('INSERT INTO videos (filename, title, description, date, category, grade, auteur, auteur_id, validation) 
+														VALUES (:filename, :title, :description, now(), :category, :grade, :auteur, :auteur_id, :validation)');
 
 							$request->execute(
 								array(
 									'filename' => $fichier,
 									'title' => $_POST['title'],
 									'description' => $_POST['description'],
+									'category' => $_POST['cat_name'],
+									'grade' => $_POST['grade_name'],
 									'auteur' => $username,
 									'auteur_id' => $auteur_id,
 									'validation' => $valid_default,
