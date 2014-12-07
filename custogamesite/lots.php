037b4c9fc6e9e7482eefe328c3e68f2f5091207c
NathanQj <nathan.qjm@gmail.com>
2014-12-07 15:50:00 +0100

    Avancement de l'intégration:Lots (pas de maquette)
    Mise en place de la page des lots à partir du zoning, puisque pas de
    maquettes.
    
    Manque : Le classement à mettre en fonction du lot (à déterminer par
    l'équipe)

---
+++ custogamesite/lots.php
@@ -0,0 +1,19 @@
+<?php
+
+/******************************** 
+	 DATABASE & FUNCTIONS 
+********************************/
+require('config/config.php');
+require('config/session.php');
+include 'view/_head.html';
+/******************************** 
+			PROCESS 
+********************************/
+
+
+/******************************** 
+			VIEW 
+********************************/
+include 'view/_menu.php';
+include 'view/lots.html';
+include 'view/_footer.html';
